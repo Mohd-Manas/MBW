7ae656d0eb4b905b0c2ef818b30c9fe64c10bb46
Mohd-Manas <mohamedmanas2222@gmail.com>
2026-05-13 17:15:17 +0400

    For test purpose

--- MBW Law/components/leadership.tsx
+++ MBW Law/components/leadership.tsx
@@ -16,7 +16,7 @@ const leaders = [
     name: "Sanjana Sharan",
     role: "Director  & Strategic Advisor",
     image: "/images/sanjana-sharan.jpeg",
-    description: "Brings a strategic, client-focused perspective with expertise in business advisory, client relationship management, and operational structuring. She drives client engagement, strategic planning, and seamless delivery of legal and business solutions - combining strategic thinking with operational efficiency.",
+    description: "Brings a strategic, client focused perspective with expertise in business advisory, client relationship management, and operational structuring. She drives client engagement, strategic planning, and seamless delivery of legal and business solutions - combining strategic thinking with operational efficiency.",
   },
 ]
 
